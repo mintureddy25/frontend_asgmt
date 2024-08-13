d07c1f6a2d7225381cd6fc1ecae1bd301294782d
Saiteja <chappetasaitejareddy@gmail.com>
2024-08-14 00:26:40 +0530

    added mainpage and graph

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Graph from '../src/components/Areachart';
+import Example from '../src/components/tabs';
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+    <div className='bg-white'>
+      <div className='mx-auto max-w-7xl px-6 py-24 sm:pt-32 lg:px-8 lg:py-40'> 
+    <Example />
+    <Graph />
+
+    </div>
+    </div>
+    </div>
   )
 }
