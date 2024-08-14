bece6cede3663c0e55528fe435073deaa12e3260
Saiteja <chappetasaitejareddy@gmail.com>
2024-08-14 18:16:37 +0530

    added browser routes including 404 page not found error

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import Dashboard from "./components/Dashboard";
+import { Route, Routes } from "react-router-dom";
+import PageNotFound from "./utils/pageNotFound";
 
 export default function App() {
   return (
-    <div>
-      <Dashboard />
-    </div>
+    <>
+      <Routes>
+        <Route path="/" Component={Dashboard} />
+        <Route path="/Dashboard" Component={Dashboard} />
+        <Route path="*" Component={PageNotFound} />
+      </Routes>
+    </>
   );
 }
